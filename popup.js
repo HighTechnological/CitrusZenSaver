91b49c7a4a30920514514d8a41e7caac058c0f4d
HighTechnological <moslienko@icloud.com>
2016-07-03 16:33:30 +0700

    0.1.1
    Запрос по нажатию, а не автоматически.
    Подправлен UI
    Комментарии

--- popup.js
+++ popup.js
@@ -1,32 +1,37 @@
 window.onload = function() {
-  //Получает URL текущей вкладки
-  chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true
-}, function(tabs) {
-  // and use that tab to fill in out title and url
-  var tab = tabs[0];
-  console.log(tab.url);
-  //alert(tab.url);
+  // Когда расширение открыто, получает имя пользователя
   var xhr = new XMLHttpRequest();
-
-  var note = {
-  link: "Tom1"
-  };
-
-  var body = "save=" + tab.url;
-
-
-  xhr.open("GET", "http://citrus.ru.com/api.php?" +body , true);
+  xhr.open("GET", "http://citrus.ru.com/api.php?user=name" , true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
        if(xhr.status == 200) {
          document.getElementById('result').innerHTML = xhr.responseText;
-
            }
     }
-
   }
   xhr.send();
+  // Дальше получает URL текущей вкладки
+  chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true
+}, function(tabs) {
+  var tab = tabs[0];
+  //console.log(tab.url); // Сообщение в консоль
+  var xhr = new XMLHttpRequest();
+  // URL сохраняется в переменную с запросом
+  var body = "save=" + tab.url;
+  // При нажатии кнопки отправить запрос
+  document.getElementById('saveLink').onclick = function() {
+  // Используя CitrusZen API
+  xhr.open("GET", "http://citrus.ru.com/api.php?" +body , true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+       if(xhr.status == 200) { //Если все в порядке, выводит ответ
+         document.getElementById('result').innerHTML = xhr.responseText;
+           }
+    }
+  }
+  xhr.send();
+}
 });
 }
